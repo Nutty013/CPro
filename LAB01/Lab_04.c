ed2df2dfa5aaef8fc2ec9c53c02cfc205159da58
Nutty013 <nutcha.kal@ku.th>
2026-09-08 16:16:30 +0700

    8/9

---
+++ LAB01/Lab_04.c
@@ -0,0 +1,9 @@
+/* File Name: Lab_04.c */
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	printf("\"Kasetsart University\"\n");
+	printf("\t\'Kasetsart University\'\n");
+	printf("\t\t\**Faculty of Liberal Arts and Sciences\**\n");
+}
